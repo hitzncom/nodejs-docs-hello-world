2990072e93a36c1e056eccd17ddceed3061bc4ec
hitzncom <74599621+hitzncom@users.noreply.github.com>
2020-11-18 21:39:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello LEE! & GOOD ");
+    response.end("Hello LEE! & GOOD GOOD");
 });
 
 //const port = process.env.PORT || 1337;
